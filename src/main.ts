f94954cbdb875e911fc988021929edfbae626fd5
Glenn <phoobynet@users.noreply.github.com>
2023-03-01 12:47:06 +0000

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import App from '@/App.vue'
 import { router } from '@/router'
 import '@/styles/style.scss'
-import { MotionPlugin } from '@vueuse/motion'
 import { createApp } from 'vue'
 
 const app = createApp(App)
 
-app.use(MotionPlugin)
 app.use(router)
 app.mount('#app')
